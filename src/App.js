f4563edcb3dd657d3f295238fb649b7ce3b484a5
Hamza Khan <hamza91394@gmail.com>
2020-05-05 21:37:34 -0400

    Deploying

--- src/App.js
+++ src/App.js
@@ -4,50 +4,131 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Signin from './components/Signin/Signin'
+import Register from './components/Register/Register'
 import './App.css';
 import 'tachyons';
-import Clarifai from 'clarifai';
 
-const app = new Clarifai.App({
-  apiKey: "13cc32a6ab304cac9e6511788678ca04"
-})
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '', 
+    entries: 0,
+    joined: ''
+  }
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: ''
+    this.state = initialState
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email, 
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
 
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({input: event.target.value})
   }
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function(response){
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err){
-
+    fetch('https://shrouded-wave-31054.herokuapp.com/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                input: this.state.input
+            })
+        })
+    .then(response => response.json())
+    .then(response => {
+      if (response) {
+        fetch('https://shrouded-wave-31054.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id
+            })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+        })
+        .catch(console.log)
       }
-    );
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err))
+  }
+
+  onRouteChange = (route) => {
+    if (route==='home'){
+      this.setState({isSignedIn: true})
+    } else if (route === 'signout') {
+      this.setState(initialState)
+    }
+    this.setState({route: route})
+  }
+
+  renderSwitch = (route) => {
+    switch(route){
+      case 'home':
+        return <div>
+          <Logo/>
+          <Rank name={this.state.user.name} entries={this.state.user.entries} />
+          <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        </div>
+      case 'signin':
+      case 'signout':
+        return <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+      case 'register':
+        return <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+      default:
+        return;
+    }
   }
 
   render() {
     return(
       <div className="App">
-        <Navigation/>
-        <Logo/>
-        <Rank/>
-        <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl = {this.state.imageUrl}/>
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn}/>
+        {this.renderSwitch(this.state.route)}
       </div>
     )
   }
